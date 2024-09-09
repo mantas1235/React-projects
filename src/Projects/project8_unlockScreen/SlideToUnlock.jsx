38f58ffdaa21174271b9eb9500ebc2ff6a7557c3
mantas1235 <mantas.san@gmail.com>
2024-09-09 19:11:50 +0300

    Create useState for lockSlider

--- src/Projects/project8_unlockScreen/SlideToUnlock.jsx
+++ src/Projects/project8_unlockScreen/SlideToUnlock.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import LockSlider from './LockSlider'
-
+import { AiFillUnlock } from 'react-icons/ai'
+import { useState } from 'react'
 
 
 
@@ -9,8 +10,31 @@ import LockSlider from './LockSlider'
 
 
 function SlideToUnlock() {
+
+const [showLockSlider, setShowLockSlider] = useState(true)
+
+
+
   return (
-    <div><LockSlider /></div>
+    <div className='container text-center d-flex flex-column border-20' style={{
+        height: "90vh",
+        marginTop: "10vh",
+        width: 280,
+        border: "4px, solid #000"
+
+    }}>
+        
+        <h1 className='title'>Title</h1>
+
+{
+    showLockSlider ? (<LockSlider width={"250px"} />) : (<AiFillUnlock className='unlockIcon' />)
+}
+
+        
+        
+        
+        
+        </div>
   )
 }
 
