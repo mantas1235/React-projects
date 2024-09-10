c97897aebfd2436a470c6cc51bb0576a21ce4518
mantas1235 <mantas.san@gmail.com>
2024-09-10 11:06:23 +0300

    Finish procect 8

--- src/Projects/project8_unlockScreen/SlideToUnlock.jsx
+++ src/Projects/project8_unlockScreen/SlideToUnlock.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import LockSlider from './LockSlider'
 import { AiFillUnlock } from 'react-icons/ai'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import LockScreenImg from './img/img_0002s_0001_lost-valley-2.jpg'
 import HomeScreenImg from './img/img_0003s_0000_marvel-1.jpg'
 
@@ -26,10 +26,34 @@ const [lockSliderValue, setLockSliderValue] = useState(0)
 
 const handleLockSlider = (e) => {
 setLockSliderValue(e.target.value)
-console.log(e.target.value);
-setShowLockSlider(false)
+
+
 }
 
+const lockScreen = () => {
+    setLockSliderValue(0)
+    setShowLockSlider(true)
+    setUiProps({
+        uiText: 'Unlock screen',
+        uiColor: '#000 ',
+        uiBg: `url(${LockScreenImg}) center/cover`
+    })
+}
+
+
+useEffect(() => {
+document.body.style.background = '#999'
+if (lockSliderValue==="100") {
+    setShowLockSlider(false)
+    setLockSliderValue(0)
+setUiProps({
+    uiText: 'Lock screen',
+    uiColor: '#222 ',
+    uiBg: `url(${HomeScreenImg}) center/cover`
+})
+}
+
+}, [lockSliderValue])
 
   return (
     <div className='container text-center d-flex flex-column border-20' style={{
@@ -43,7 +67,7 @@ background: uiProps.uiBg,
         <h1 className='title' style={{color: uiProps.uiColor}}>{uiProps.uiText}</h1>
 
 {
-    showLockSlider ? (<LockSlider handleInput={handleLockSlider} value={lockSliderValue} width={"250px"} />) : (<AiFillUnlock className='unlockIcon' />)
+    showLockSlider ? (<LockSlider handleInput={handleLockSlider} sliderValue={lockSliderValue} width={"250px"} />) : (<AiFillUnlock className='unlockIcon' onClick={lockScreen}/>)
 }
 
         
